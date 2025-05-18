79577d30dbe1021a9dcbf060787276ca520de670
Robin Chae <150565578+blisssr77@users.noreply.github.com>
2025-05-17 20:51:24 -0700

    fixing tailwindcss

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline text-blue-600 p-8">
+      🎉 Tailwind is working!
+    </div>
+  );
+}
+
+export default App;
